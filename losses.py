89a588ad2d850a269ee0dbdb1bd2aaf6b1401610
Maurits Diephuis <mdiephuis@gmail.com>
2019-06-05 18:10:26 +0200

    added cuda support for dog pyr loss

--- losses.py
+++ losses.py
@@ -96,7 +96,7 @@ def loss_infovae(x, x_hat, z_mu, z_std, alpha, beta, use_cuda, gamma=1.0):
     return total_loss
 
 
-def gauss_kernel(k_size=5, sigma=1.0):
+def gauss_kernel(k_size=5, sigma=1.0, use_cuda=False):
     if k_size % 2 != 1:
         raise ValueError("kernel size must be uneven")
 
@@ -108,12 +108,13 @@ def gauss_kernel(k_size=5, sigma=1.0):
     kernel = torch.exp(- distsq / denom)
     kernel /= torch.sum(kernel)
     kernel = torch.reshape(kernel, (k_size, k_size))
+    kernel = kernel.cuda() if use_cuda else kernel
     return kernel
 
 
-def dog_pyramid(im, n_levels=5, k_size=15, sigma=4.0):
+def dog_pyramid(im, n_levels=5, k_size=15, sigma=4.0, use_cuda=False):
 
-    kernel = gauss_kernel(k_size, sigma)
+    kernel = gauss_kernel(k_size, sigma, use_cuda)
     kernel = kernel.unsqueeze(0).unsqueeze(0)
 
     func_pad = nn.ReplicationPad2d((k_size // 2, k_size // 2, k_size // 2, k_size // 2))
@@ -129,9 +130,9 @@ def dog_pyramid(im, n_levels=5, k_size=15, sigma=4.0):
     return log_pyr
 
 
-def loss_dog(im1, im2, n_levels=5, k_size=5, sigma=2.0):
-    pyramid_im1 = dog_pyramid(im1, n_levels, k_size, sigma)
-    pyramid_im2 = dog_pyramid(im2, n_levels, k_size, sigma)
+def loss_dog(im1, im2, n_levels=5, k_size=5, sigma=2.0, use_cuda=False):
+    pyramid_im1 = dog_pyramid(im1, n_levels, k_size, sigma, use_cuda)
+    pyramid_im2 = dog_pyramid(im2, n_levels, k_size, sigma, use_cuda)
     return sum(F.l1_loss(a, b) for a, b in zip(pyramid_im1, pyramid_im2)) / n_levels
 
 
